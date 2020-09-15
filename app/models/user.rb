18470e09ee87754be7e5d4c4dc0ad3139e72d925
taimushimizu <213.f.r48132@gmail.com>
2020-09-15 14:17:05 +0900

    Userのリレーションを記述

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,12 @@
 class User < ApplicationRecord
+  has_many :active_reactions, class_name: "Reaction",
+                              foreign_key: "from_user_id",
+                              dependent: destroy
+  has_many :likes through :active_reactions, source: :to_user
+  has_many :passive_reactions, class_name: "Reaction",
+                               foreign_key: "to_user_id",
+                               dependent: destroy
+  has_many :enlikes through :passive_reactions, source: :fron_user
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
